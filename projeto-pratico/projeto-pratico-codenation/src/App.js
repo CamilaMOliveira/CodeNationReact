3561bf74b06521fa13b33f34f0894a0584e37b82
Camila Oliveira <camila.steffan@gmail.com>
2020-05-18 00:27:15 -0300

    ProjetoFinal: Adicionando o header do site com o nome da loja, e os ícones de busca e do carrinho. Fazendo uso do font awesome e fontes do Google.

--- projeto-pratico/projeto-pratico-codenation/src/App.js
+++ projeto-pratico/projeto-pratico-codenation/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
 
+/* BEM CSS => block__element--modifier 
+  * Block: The outermost parent element of the component is defined as the block.
+  * Element: Inside of the component may be one or more children called elements.
+  * Modifier: Either a block or element may have a variation signified by a modifier
+*/
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="App-nav">
+          <a className="App-nav__title" href="/">Fashionista</a>
+          <div className="App-nav__icons">
+            <button class="App-nav__icons--search">
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+            <button class="App-nav__icons--cart">
+              <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            </button>
+          </div>
+        </nav>
+        {/* <div class="App-header__title">Fashionista</div>
+        <div class="App-header__icons">Search Carrinho</div> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,8 +36,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+        </header>
     </div>
   );
 }
