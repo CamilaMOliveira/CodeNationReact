2489e74aa0c25430d1279686496d6b48b81aae46
Camila Oliveira <camila.steffan@gmail.com>
2020-05-20 19:37:10 -0300

    Challenge-7: Instalando dependências para utilizar o font-awesome; Adicionando código para a Topbar do aplicativo.

--- react-15/src/components/Topbar/Topbar.jsx
+++ react-15/src/components/Topbar/Topbar.jsx
@@ -4,11 +4,30 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as LogoSvg } from '../../assets/img/instagram-logo.svg';
 
+import 'font-awesome/css/font-awesome.min.css';
+
 import './Topbar.scss';
 
 const Topbar = () => (
   <header className="topbar">
     <div className="container">
+      <a href="/" className="topbar__logo">
+        <LogoSvg/>
+      </a>
+      <div className="topbar__group">
+        <div className="topbar__icon">
+          <a href="/">
+            <i class="fas fa-users" aria-hidden="true"></i>
+            <span>Usuários</span>
+          </a>
+        </div>
+        <div className="topbar__icon">
+          <a href="/">
+            <i class="fas fa-user-plus" aria-hidden="true"></i>
+            <span>Nova Conta</span>
+          </a>
+        </div>
+      </div>
     </div>
   </header>
 );
