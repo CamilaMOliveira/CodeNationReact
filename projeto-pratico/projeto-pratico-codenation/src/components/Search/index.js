d97089a62d8cc2bf29e5000a77a4f58c40d09968
Camila Oliveira <camila.steffan@gmail.com>
2020-06-07 22:21:40 -0300

    ProjetoFinal - Iniciando a página de busca.

--- projeto-pratico/projeto-pratico-codenation/src/components/Search/index.js
+++ projeto-pratico/projeto-pratico-codenation/src/components/Search/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import '../../App.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { Link } from "react-router-dom";
+import Cards from "../Cards";
+
+let handleInput = (event) => {
+  console.log(`Usuario digitou = ${event.target.value}`);
+}
 
 function Search () {
   return (
-    <h1> SEARCH </h1>
+    <div class="search">
+      {/* <h1> SEARCH </h1> */}
+      <input class="search__input" type="text" placeholder="O que você procura?"
+             onInput={(event) => handleInput(event)}>
+
+      </input>
+      <Cards/>
+    </div>
   );
 }
 
